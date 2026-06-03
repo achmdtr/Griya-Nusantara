4448b9325092a79a5b8af04c4a629774ab38f0ed
sat <wongsolo572@gmail.com>
2026-06-03 10:02:21 +0700

    final

--- lib/house_detail_screen.dart
+++ lib/house_detail_screen.dart
@@ -4,7 +4,6 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'app_colors.dart';
 import 'quiz_confirmation_screen.dart';
-import 'quiz_screen.dart';
 import 'utils/responsive_helper.dart';
 
 class HouseDetailScreen extends StatefulWidget {
